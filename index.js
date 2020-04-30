8c036f221538b270315a0901696e9c114d7929a8
yyhugh <hjyuanyy@gmail.com>
2020-04-30 14:54:27 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
 require(["m"], function (myModule) {
+    console.log("myModule", myModule);
+    var name = myModule.getName();
+    console.log(name);
+});
+
+
+require(["m", "m"], function (myModule) {
     console.log("myModule", myModule);
     var name = myModule.getName();
     console.log(name);
